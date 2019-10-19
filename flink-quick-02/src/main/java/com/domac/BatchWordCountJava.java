09693f8af0c52d8a2e34a48fd38a827db91727b2
domac <domacode@gmail.com>
2019-10-19 18:28:59 +0800

    fix batch

--- flink-quick-02/src/main/java/com/domac/BatchWordCountJava.java
+++ flink-quick-02/src/main/java/com/domac/BatchWordCountJava.java
@@ -8,14 +8,17 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.util.Collector;
 
+import java.io.File;
+
 public class BatchWordCountJava {
 
     public static void main(String[] args) throws Exception {
         String input = "";
-
+        String output = "";
         try {
             ParameterTool parameterTool = ParameterTool.fromArgs(args);
             input = parameterTool.get("input");
+            output = parameterTool.get("output");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -24,6 +27,25 @@ public class BatchWordCountJava {
             input = "/tmp/batch_in";
         }
 
+        if (null == output || output.isEmpty()) {
+            output = "/tmp/batch_out";
+        }
+
+        File file = new File(output);
+
+        try {
+            if (file.exists()) {
+                if(file.delete()){
+                    System.out.println(file.getName() + " 文件已被删除！");
+                }else{
+                    System.out.println("文件删除失败！");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         //获取运行环境
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
@@ -31,10 +53,10 @@ public class BatchWordCountJava {
         DataSource<String> text = env.readTextFile(input);
 
         //批处理sink
-        DataSet<Tuple2<String, Long>> dataSet = text.flatMap(new BatchTokenizer());
-        dataSet.print();
+        DataSet<Tuple2<String, Long>> dataSet = text.flatMap(new BatchTokenizer()).groupBy(0).sum(1);
+        dataSet.writeAsCsv(output, "\n", "").setParallelism(1);
         //延迟执行
-        //env.execute("my batch word count application");
+        env.execute("my batch word count application");
     }
 
 
