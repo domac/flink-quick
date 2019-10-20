137178c037c5c1244c583d4516fc9f67f3a4f4e4
domac <domacode@gmail.com>
2019-10-20 12:05:22 +0800

    增加自定义的kafka消息类型

--- flink-quick-05/src/main/java/com/domac/SimpleStreamingFromKafka.java
+++ flink-quick-05/src/main/java/com/domac/SimpleStreamingFromKafka.java
@@ -1,10 +1,14 @@
 package com.domac;
 
+import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -27,13 +31,53 @@ public class SimpleStreamingFromKafka {
         properties.setProperty("group.id", kafkaGroup);
 
         //创建Kafka Source
-        FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<String>(
-                kafkaTopic, new SimpleStringSchema(), properties);
+        //FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<String>(kafkaTopic, new SimpleStringSchema(), properties);
+        //DataStream<String> stream = env.addSource(flinkKafkaConsumer);
 
-        DataStream<String> stream = env.addSource(flinkKafkaConsumer);
+        //自定义Schema的方式
+        FlinkKafkaConsumer<Message> flinkKafkaConsumer = new FlinkKafkaConsumer<>(kafkaTopic, new MessageDeserializer(), properties);
+        DataStream<Message> stream = env.addSource(flinkKafkaConsumer);
 
         stream.print();
-
         env.execute("simple streaming from kafka");
+    }
+
+    //自定义消息格式
+    public static class Message {
+        String name;
+        String data;
+
+        public Message(String name, String data) {
+            this.name = name;
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "Message{" +
+                    "topic='" + name + '\'' +
+                    ", data='" + data + '\'' +
+                    '}';
+        }
+    }
+
+    public static class MessageDeserializer implements DeserializationSchema<Message> {
+
+        @Override
+        public Message deserialize(byte[] bytes) throws IOException {
+            //这里只是简单的反序列化为字符串,当然你也可以反序列化为JSON
+            String message = new String(bytes);
+            return new Message("my_test_topic", message);
+        }
+
+        @Override
+        public boolean isEndOfStream(Message message) {
+            return false;
+        }
+
+        @Override
+        public TypeInformation<Message> getProducedType() {
+            return TypeExtractor.getForClass(Message.class);
+        }
     }
 }
